cc447b17b0f901057d04166511bed18d4e055e3b
WG37 <willg2070@gmail.com>
2026-03-07 12:48:01 +1100

    feat(): add removeById method & remove bool on return

--- Weardian.Server/Application/Interfaces/ISymmetricKeyRepository.cs
+++ Weardian.Server/Application/Interfaces/ISymmetricKeyRepository.cs
@@ -1,8 +1,12 @@
-﻿namespace Weardian.Server.Application.Interfaces
+﻿using Weardian.Server.Domain.Keys.SymmetricKeys;
+
+namespace Weardian.Server.Application.Interfaces
 {
     public interface ISymmetricKeyRepository
     {
-        public Task<bool> AddAsync();
-        public Task GetById();
+        public Task AddAsync(SymmetricKey entity);
+        public Task<IEnumerable<SymmetricKey>> GetAllAsync();
+        public Task<SymmetricKey> GetByIdAsync(Guid publicId);
+        public Task RemoveByIdAsync(Guid publicId);
     }
 }
---
+++ Weardian.Server/Infrastructure/Repository/SymmetricKeyRepository/SymmetricKeyRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Eventing.Reader;
+using Weardian.Server.Application.Interfaces;
+using Weardian.Server.Domain.Keys.SymmetricKeys;
+using Weardian.Server.Infrastructure.Data;
+
+namespace Weardian.Server.Infrastructure.Repository.SymmetricKeyRepository
+{
+    public class SymmetricKeyRepository : ISymmetricKeyRepository
+    {
+        private readonly AppDbContext _db;
+        public SymmetricKeyRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task AddAsync(SymmetricKey entity)
+        {
+            _db.SymmetricKeys.Add(entity);
+
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<SymmetricKey> GetByIdAsync(Guid publicId)
+        {
+            var entity = await _db.SymmetricKeys.SingleOrDefaultAsync(k => k.Id == publicId);
+            if (entity == null)
+                throw new KeyNotFoundException("publicId does not exist on database");
+
+            return entity;    
+        }
+
+        public async Task<IEnumerable<SymmetricKey>> GetAllAsync()
+        {
+            var entities = await _db.SymmetricKeys.ToListAsync();
+            if (entities == null)
+                throw new KeyNotFoundException("No keys exist on the database");
+
+            return entities;
+        }
+
+        public async Task RemoveByIdAsync(Guid publicId)
+        {
+            var entity = await _db.SymmetricKeys.SingleOrDefaultAsync(k => k.Id == publicId);
+            if (entity == null)
+                throw new KeyNotFoundException($"ID: {publicId} does not exist on database");
+
+            _db.SymmetricKeys.Remove(entity);
+            await _db.SaveChangesAsync();
+        }
+
+    }
+}
