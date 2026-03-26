8f9c405977b6c554cfdffa6ea27ba9461407e099
WG37 <willg2070@gmail.com>
2026-03-26 15:03:33 +1100

    feat(): ef migrations reflect new property name changes

---
+++ Weardian.Server/Migrations/20260326035459_ChangePublicIdToEnvelopeId.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Weardian.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePublicIdToEnvelopeId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublicId",
+                table: "SymmetricKeyRecords",
+                newName: "EnvelopeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SymmetricKeyRecords_PublicId",
+                table: "SymmetricKeyRecords",
+                newName: "IX_SymmetricKeyRecords_EnvelopeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EnvelopeId",
+                table: "SymmetricKeyRecords",
+                newName: "PublicId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_SymmetricKeyRecords_EnvelopeId",
+                table: "SymmetricKeyRecords",
+                newName: "IX_SymmetricKeyRecords_PublicId");
+        }
+    }
+}
