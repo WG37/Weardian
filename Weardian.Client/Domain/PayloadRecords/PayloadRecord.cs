3516140384f42661ddcba69f2d810f3087e31118
WG37 <willg2070@gmail.com>
2026-03-26 15:00:12 +1100

    feat(): add new entity PayloadRecord for local storage

---
+++ Weardian.Client/Domain/PayloadRecords/PayloadRecord.cs
@@ -0,0 +1,19 @@
+﻿namespace Weardian.Client.Domain.PayloadRecords
+{
+    internal class PayloadRecord
+    {
+        public ReadOnlyMemory<byte> Ciphertext { get; private set; }
+        public Guid EnvelopeId { get; init; }
+
+        public byte[] Nonce { get; init; }
+        public byte[] Tag { get; init; }
+
+        public PayloadRecord(byte[] ciphertext)
+        {
+            if (ciphertext == null || ciphertext.Length < 16)
+                throw new ArgumentException("Ciphertext must be at least 16 bytes or larger.");
+
+            Ciphertext = new ReadOnlyMemory<byte>((byte[])ciphertext.Clone());
+        }
+    }
+}
