91aa978be7d76d17ae6f192dc92d5db37c1f78a0
WG37 <willg2070@gmail.com>
2026-04-19 11:42:07 +1000

    feat(): add GetById implementation

--- Weardian.Client/Infrastructure/Repositories/SymmetricKeyRepository.cs
+++ Weardian.Client/Infrastructure/Repositories/SymmetricKeyRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System.IO;
 using System.Text.Json;
 using Weardian.Client.Core.DTOs.CryptographyDtos;
-using Weardian.Client.Core.DTOs.KeyDtos;
 using Weardian.Client.Core.Interfaces;
 using Weardian.Client.Domain.KeyRecords.Symmetric;
 using Weardian.Client.Domain.PayloadRecords;
@@ -12,7 +11,6 @@ namespace Weardian.Client.Infrastructure.Repositories
 {
     internal class SymmetricKeyRepository : ISymmetricKeyRepository
     {
-
         public async Task AddLocalRecordsAsync(SymmetricKeyRecord keyRecord, PayloadRecord payloadRecord)
         {
             var keyPath = AppDataPaths.KeyRecordPath(keyRecord.EnvelopeId);
@@ -28,9 +26,58 @@ namespace Weardian.Client.Infrastructure.Repositories
         public async Task<IReadOnlyList<EncryptedPayloadRecordDto>> GetLocalPayloadRecordsAsync()
         {
             if (!Directory.Exists(AppDataPaths.BlobsDir))
-                return [];
+                throw new InvalidOperationException("No local record directory exists.");
 
             var payloadFiles = Directory.EnumerateFiles(AppDataPaths.BlobsDir, "*.blob");
+            var results = new List<EncryptedPayloadRecordDto>();
+
+            foreach (var payload in payloadFiles)
+            {
+                var json = await File.ReadAllTextAsync(payload);
+                var payloadRecord = JsonSerializer.Deserialize<PayloadRecord>(json);
+
+                if (payloadRecord == null)
+                    continue;
+
+                var payloadDto = new EncryptedPayloadRecordDto(
+                    EnvelopeId: payloadRecord.EnvelopeId,
+                    Name: payloadRecord.Name,
+                    Algorithm: payloadRecord.Algorithm,
+                    Ciphertext: payloadRecord.Ciphertext.ToArray(),
+                    Nonce: payloadRecord.Nonce,
+                    Tag: payloadRecord.Tag,
+                    CreatedOn: payloadRecord.CreatedOn);
+
+                results.Add(payloadDto);
+            }
+            
+            return results;
+        }
+
+        public async Task<EncryptedPayloadRecordDto> GetLocalPayloadRecordById(Guid payloadId)
+        {
+            if (!Directory.Exists(AppDataPaths.BlobsDir))
+                throw new InvalidOperationException("No local record directory exists.");
+
+            var payloadFile = AppDataPaths.BlobPath(payloadId);
+
+            if (File.Exists(payloadFile))
+                throw new FileNotFoundException($"Payload not found for the id {payloadId}.");
+
+            var json = await File.ReadAllTextAsync(payloadFile);
+            var payloadRecord = JsonSerializer.Deserialize<PayloadRecord>(json);
+
+            if (payloadRecord == null)
+                throw new InvalidDataException($"Payload file is invalid: {payloadId}");
+
+            return new EncryptedPayloadRecordDto(
+                EnvelopeId: payloadRecord.EnvelopeId,
+                Name: payloadRecord.Name,
+                Algorithm: payloadRecord.Algorithm,
+                Ciphertext: payloadRecord.Ciphertext.ToArray(),
+                Nonce: payloadRecord.Nonce,
+                Tag: payloadRecord.Tag,
+                CreatedOn: payloadRecord.CreatedOn);
         }
     }
 }
