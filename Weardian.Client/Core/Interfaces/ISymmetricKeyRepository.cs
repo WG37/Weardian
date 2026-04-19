98c3cb79aae73841c633ed8e9c65474a0a81173f
WG37 <willg2070@gmail.com>
2026-04-19 11:40:40 +1000

    feat(): add GetById interface signature

--- Weardian.Client/Core/Interfaces/ISymmetricKeyRepository.cs
+++ Weardian.Client/Core/Interfaces/ISymmetricKeyRepository.cs
@@ -8,6 +8,7 @@ namespace Weardian.Client.Core.Interfaces
     {
         public Task AddLocalRecordsAsync(SymmetricKeyRecord keyRecord, PayloadRecord payloadRecord);
         public Task<IReadOnlyList<EncryptedPayloadRecordDto>> GetLocalPayloadRecordsAsync();
+        public Task<EncryptedPayloadRecordDto> GetLocalPayloadRecordById(Guid payloadId);
 
     }
 }
