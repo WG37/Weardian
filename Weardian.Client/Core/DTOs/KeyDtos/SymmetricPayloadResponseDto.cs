eb3f1f3dcb4b8216fc30ca6073d089456d17c9a7
WG37 <willg2070@gmail.com>
2026-04-23 14:29:59 +1000

    refactor(): changed record name to reflect its purpose

--- Weardian.Client/Core/DTOs/KeyDtos/SymmetricPayloadResponseDto.cs
+++ Weardian.Client/Core/DTOs/KeyDtos/SymmetricPayloadResponseDto.cs
@@ -3,7 +3,7 @@ using Weardian.Client.Domain.Enums;
 
 namespace Weardian.Client.Core.DTOs.KeyDtos
 {
-    internal sealed record SymmetricPayloadResponseDto(
+    internal sealed record EncryptedPayloadRecord(
         string Name,
         KeyStatus KeyStatus,
         EncryptedEnvelopeDto Envelope,
