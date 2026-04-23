d51265c8e1ad9315756ffe515450dddb1f93ebde
WG37 <willg2070@gmail.com>
2026-04-23 14:32:38 +1000

    refactor(): Refactor interfaces to use payload records, not keys

--- Weardian.Client/Core/Interfaces/ISymmetricKeyManagementService.cs
+++ Weardian.Client/Core/Interfaces/ISymmetricKeyManagementService.cs
@@ -1,12 +1,12 @@
-﻿using Weardian.Client.Core.DTOs.KeyDtos;
+﻿using Weardian.Client.Core.DTOs.CryptographyDtos;
 
 namespace Weardian.Client.Core.Interfaces
 {
     internal interface ISymmetricKeyManagementService
     {
         public Task CreateEncryptedPasswordAsync(string keyName, string password);
-        public Task<SymmetricPayloadResponseDto> GetKeyByIdAsync(Guid localId);
-        public Task<SymmetricPayloadResponseDto> GetKeysAsync();
-        public Task<bool> RemoveKeyById(Guid localId);
+        public Task<IReadOnlyList<EncryptedPayloadRecordDto>> GetPayloadRecordsAsync();
+        public Task<EncryptedPayloadRecordDto> GetPayloadRecordByIdAsync(Guid envelopeId);
+        public bool RemoveRecordById(Guid envelopeId);
     }
 }
--- Weardian.Client/Core/Interfaces/ISymmetricKeyRepository.cs
+++ Weardian.Client/Core/Interfaces/ISymmetricKeyRepository.cs
@@ -1,5 +1,4 @@
-﻿using Weardian.Client.Core.DTOs.CryptographyDtos;
-using Weardian.Client.Domain.KeyRecords.Symmetric;
+﻿using Weardian.Client.Domain.KeyRecords.Symmetric;
 using Weardian.Client.Domain.PayloadRecords;
 
 namespace Weardian.Client.Core.Interfaces
@@ -8,8 +7,8 @@ namespace Weardian.Client.Core.Interfaces
     {
         public Task AddLocalRecordsAsync(SymmetricKeyRecord keyRecord, PayloadRecord payloadRecord);
         public Task<IReadOnlyList<PayloadRecord>> GetLocalPayloadRecordsAsync();
-        public Task<PayloadRecord> GetLocalPayloadRecordById(Guid payloadId);
-        public bool RemoveLocalPayloadRecordById(Guid payloadId);
+        public Task<PayloadRecord> GetLocalPayloadRecordByIdAsync(Guid envelopeId);
+        public bool RemoveLocalPayloadRecordById(Guid envelopeId);
 
     }
 }
