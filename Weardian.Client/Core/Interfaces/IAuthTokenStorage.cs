ade0ae65d5ade889771bd880bab5e572f9e806ba
WG37 <willg2070@gmail.com>
2026-04-29 17:02:49 +1000

    feat: add AuthTokenStorage contract provides methods for getting, setting, and clearing access tokens

---
+++ Weardian.Client/Core/Interfaces/IAuthTokenStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace Weardian.Client.Core.Interfaces
+{
+    public interface IAuthTokenStorage
+    {
+        public Task<string?> GetAccessTokenAsync();
+        public Task SetAccessTokenAsync(string accessToken);
+        public Task ClearAccessTokenAsync();
+    }
+}
---
+++ Weardian.Client/Core/Services/AuthTokenStorage.cs
@@ -0,0 +1,28 @@
+﻿using Weardian.Client.Core.Interfaces;
+
+namespace Weardian.Client.Core.Services
+{
+    public class AuthTokenStorage : IAuthTokenStorage
+    {
+        private string? _accessToken;
+
+        public Task<string?> GetAccessTokenAsync()
+        {
+            return Task.FromResult(_accessToken);
+        }
+
+        public Task SetAccessTokenAsync(string accessToken)
+        {
+            _accessToken = accessToken;
+
+            return Task.CompletedTask;
+        }
+
+        public Task ClearAccessTokenAsync()
+        {
+            _accessToken = null;
+
+            return Task.CompletedTask;
+        }
+    }
+}
