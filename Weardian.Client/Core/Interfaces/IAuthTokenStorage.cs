63dcd6c72e96c89f57314cd107abaf0f15cb9fec
WG37 <willg2070@gmail.com>
2026-05-02 12:27:05 +1000

    ignore

--- Weardian.Client/Core/Interfaces/IAuthTokenStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Weardian.Client.Core.Interfaces
-{
-    public interface IAuthTokenStorage
-    {
-        public Task<string?> GetAccessTokenAsync();
-        public Task SetAccessTokenAsync(string accessToken);
-        public Task ClearAccessTokenAsync();
-    }
-}
