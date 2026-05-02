91f78db50e747feaa38298582bcc416dbcf7df1b
WG37 <willg2070@gmail.com>
2026-05-02 12:19:43 +1000

    fix: add sealed key word to EncryptedPayloadRecordDto

--- Weardian.Client/Core/DTOs/CryptographyDtos/EncryptedPayloadRecordDto.cs
+++ Weardian.Client/Core/DTOs/CryptographyDtos/EncryptedPayloadRecordDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Weardian.Client.Core.DTOs.CryptographyDtos
+﻿namespace Weardian.Client.Core.DTOs.CryptographyDtos
 {
     public sealed record EncryptedPayloadRecordDto(
         Guid EnvelopeId,
