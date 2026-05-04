1737d83b32ba6f40b29754a3a66197a76e454eac
WG37 <willg2070@gmail.com>
2026-05-04 19:39:40 +1000

    feat: add AuthService interface and AuthService implementation includes methods for register, login, and logout

---
+++ Weardian.Client/Core/Interfaces/Auth/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Weardian.Client.Core.DTOs.AuthDtos.Requests;
+using Weardian.Client.Core.DTOs.AuthDtos.Responses;
+
+namespace Weardian.Client.Core.Interfaces.Auth
+{
+    public interface IAuthService
+    {
+        public Task RegisterUserAsync(string email, string username, string password);
+        public Task LoginAsync(string email, string password);
+        public Task LogoutAsync();
+    }
+}
---
+++ Weardian.Client/Core/Services/Auth/AuthService.cs
@@ -0,0 +1,66 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using Weardian.Client.Core.DTOs.AuthDtos.Requests;
+using Weardian.Client.Core.DTOs.AuthDtos.Responses;
+using Weardian.Client.Core.Interfaces.Auth;
+using Weardian.Client.Core.Interfaces.InputValidation;
+
+namespace Weardian.Client.Core.Services.Auth
+{
+    public class AuthService : IAuthService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IAuthTokenStorage _authStorage;
+        private readonly IInputValidationService _validationService;
+
+        public AuthService(
+            HttpClient httpClient,
+            IAuthTokenStorage authStorage,
+            IInputValidationService validationService)
+        {
+            _httpClient = httpClient;
+            _authStorage = authStorage;
+            _validationService = validationService;
+        }
+
+        public async Task RegisterUserAsync(string email, string username, string password)
+        {
+            var validationResults = _validationService.ValidateRegisterUser(email, username, password);
+            
+            if (!validationResults.IsValid)
+                throw new ArgumentException(string.Join("\n", validationResults.Errors));
+
+            var registerDto = new RegisterRequestDto(email, username, password);
+
+            var response = await _httpClient.PostAsJsonAsync("/api/auth/register", registerDto);
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task LoginAsync(string email, string password)
+        {
+            var validationResults = _validationService.ValidateLogin(email, password);
+
+            if (!validationResults.IsValid)
+                throw new ArgumentException(string.Join("\n", validationResults.Errors));
+
+            var loginDto = new LoginRequestDto(email, password);
+
+            var response = await _httpClient.PostAsJsonAsync("/api/auth/login", loginDto);
+
+            response.EnsureSuccessStatusCode();
+
+            var authResponse = await response.Content.ReadFromJsonAsync<LoginResponseDto>();
+
+            if (authResponse == null || string.IsNullOrEmpty(authResponse.AccessToken))
+                throw new InvalidOperationException("Failed to authenticate: Invalid response.");
+
+            await _authStorage.SetAccessTokenAsync(authResponse.AccessToken);
+        }
+
+        public async Task LogoutAsync()
+        {
+             await _authStorage.ClearAccessTokenAsync();
+        }
+    }
+}
