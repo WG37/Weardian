78708a0932bda5597819bf0cbeaec18d35bc4996
WG37 <willg2070@gmail.com>
2026-05-05 12:55:25 +1000

    feat: registered auth, validation, KeyRecord, PayloadRecord, KeySync services

--- Weardian.Client/Presentation/App.xaml.cs
+++ Weardian.Client/Presentation/App.xaml.cs
@@ -1,17 +1,25 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
 using System.Windows;
-using Weardian.Client.Core.Interfaces;
+using Weardian.Client.Core.Interfaces.Auth;
 using Weardian.Client.Core.Interfaces.Cryptography;
 using Weardian.Client.Core.Interfaces.Cryptography.Encryption;
 using Weardian.Client.Core.Interfaces.Cryptography.KeyWrapping;
-using Weardian.Client.Core.Services;
+using Weardian.Client.Core.Interfaces.InputValidation;
+using Weardian.Client.Core.Interfaces.Symmetric;
+using Weardian.Client.Core.Interfaces.Symmetric.Repositories;
+using Weardian.Client.Core.Interfaces.Sync;
+using Weardian.Client.Core.Services.Auth;
+using Weardian.Client.Core.Services.InputValidation;
+using Weardian.Client.Core.Services.Symmetric;
+using Weardian.Client.Core.Services.Sync;
 using Weardian.Client.Infrastructure.Cryptography;
 using Weardian.Client.Infrastructure.Cryptography.Encryption;
 using Weardian.Client.Infrastructure.Cryptography.KeyWrapping;
 using Weardian.Client.Infrastructure.Native.Bootstrapper;
-using Weardian.Client.Infrastructure.Repositories;
+using Weardian.Client.Infrastructure.Repositories.Symmetric;
 
-namespace Weardian.Client
+namespace Weardian.Client.Presentation
 {
     /// <summary>
     /// Interaction logic for App.xaml
@@ -26,8 +34,9 @@ namespace Weardian.Client
 
             var services = new ServiceCollection();
 
-            services.AddScoped<ISymmetricKeyRepository, SymmetricKeyRepository>();
-            services.AddScoped<ISymmetricKeyManagementService, SymmetricKeyManagementService>();
+            services.AddScoped<IPayloadRecordRepository, PayloadRecordRepository>();
+            services.AddScoped<IKeyRecordRepository, KeyRecordRepository>();
+            services.AddScoped<IKeyManagementService, KeyManagementService>();
             services.AddScoped<IPayloadService, PayloadService>();
 
             services.AddScoped<ISymmetricCryptoService, SymmetricCryptoService>();
@@ -36,6 +45,22 @@ namespace Weardian.Client
 
             services.AddScoped<IKeyWrappingService, KeyWrappingService>();
             services.AddScoped<IKekProvider, KekProvider>();
+
+            services.AddScoped<IAuthTokenStorage, AuthTokenStorage>();
+
+            services.AddScoped<IInputValidationService, InputValidationService>();
+
+            services.AddHttpClient<IKeyRecordSyncService, KeyRecordSyncService>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:7259");
+            });
+
+            services.AddScoped<MainWindow>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
         }
     }
 
