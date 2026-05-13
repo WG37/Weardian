d97ac9942c64257e91e1f0a419646110aae2b165
WG37 <willg2070@gmail.com>
2026-05-13 17:59:49 +1000

    refactor: removed createdOn from dto

--- Weardian.Client/Core/DTOs/KeySyncingDtos/KeySyncRequestDto.cs
+++ Weardian.Client/Core/DTOs/KeySyncingDtos/KeySyncRequestDto.cs
@@ -1,13 +1,15 @@
-﻿namespace Weardian.Client.Core.DTOs.KeySyncingDtos
+﻿using Weardian.Client.Domain.Enums;
+
+namespace Weardian.Client.Core.DTOs.KeySyncingDtos
 {
     public sealed record KeySyncRequestDto(
         Guid EnvelopeId,
         string Name,
+        KeyType KeyType,
         int EnvelopeVersion,
         string WrapAlgorithm,
         Guid WrappingKeyId,
         byte[] WrappedKeyNonce,
         byte[] WrappedKeyCiphertext,
-        byte[] WrappedKeyTag,
-        DateTime CreatedOn);
+        byte[] WrappedKeyTag);
 }
