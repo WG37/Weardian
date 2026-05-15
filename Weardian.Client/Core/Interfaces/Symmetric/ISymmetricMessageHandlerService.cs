d1bf7172f8457f1635426474ae3af9ee491b2606
WG37 <willg2070@gmail.com>
2026-05-15 21:01:40 +1000

    feat: add symmetricHandlerService interface and implementation.

---
+++ Weardian.Client/Core/Interfaces/Symmetric/ISymmetricMessageHandlerService.cs
@@ -0,0 +1,9 @@
+﻿using Weardian.Client.Core.DTOs.MessageHandlerDtos;
+
+namespace Weardian.Client.Core.Interfaces.Symmetric
+{
+    public interface ISymmetricMessageHandlerService
+    {
+        public Task<string> HandleEncryptionRequestAsync(string request);
+    }
+}
---
+++ Weardian.Client/Core/Services/Symmetric/SymmetricMessageHandlerService.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using Weardian.Client.Core.DTOs.MessageHandlerDtos;
+using Weardian.Client.Core.DTOs.WebViewDtos;
+using Weardian.Client.Core.Interfaces.Symmetric;
+
+namespace Weardian.Client.Core.Services.Symmetric
+{
+    public class SymmetricMessageHandlerService : ISymmetricMessageHandlerService
+    {
+        private readonly IKeyManagementService _keyManagementService;
+
+        public SymmetricMessageHandlerService(
+            IKeyManagementService keyManagementService)
+        {
+            _keyManagementService = keyManagementService;
+        }
+
+        public async Task<string> HandleEncryptionRequestAsync(string request)
+        {
+            if (string.IsNullOrWhiteSpace(request))
+                throw new ArgumentException("Invalid request: cannot be null, empty or whitespace", nameof(request));
+
+            var result = JsonSerializer.Deserialize<EncryptionRequestDto>(request)
+                ?? throw new ArgumentNullException("The deserialized result cannot be null");
+
+            var encryptionResult = await _keyManagementService
+                .CreateEncryptedPasswordAsync(
+                    result.KeyName, 
+                    result.Password, 
+                    result.CreateSynced);
+
+            return JsonSerializer.Serialize(
+                new WebViewResponseDto<EncryptionResultDto>(
+                    Type: "encryptionResult",
+                    Success: true,
+                    Data: encryptionResult,
+                    Error: null
+                ));
+        }
+    }
+}
