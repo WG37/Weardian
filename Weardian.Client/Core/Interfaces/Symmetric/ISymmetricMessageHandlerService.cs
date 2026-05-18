d272b590d7693fdc7243ab41b6fa8061e9b07472
WG37 <willg2070@gmail.com>
2026-05-19 00:12:21 +1000

    feat: create a handlerAsync method that is responsible for matching request types from UI to invoke the proper services

--- Weardian.Client/Core/Interfaces/Symmetric/ISymmetricMessageHandlerService.cs
+++ Weardian.Client/Core/Interfaces/Symmetric/ISymmetricMessageHandlerService.cs
@@ -1,9 +1,9 @@
-﻿using Weardian.Client.Core.DTOs.MessageHandlerDtos;
-
-namespace Weardian.Client.Core.Interfaces.Symmetric
+﻿namespace Weardian.Client.Core.Interfaces.Symmetric
 {
     public interface ISymmetricMessageHandlerService
     {
+        public Task<string> HandleAsync(string requestType);
         public Task<string> HandleEncryptionRequestAsync(string request);
+        public Task<string> HandleDecryptionRequestAsync(string request);
     }
 }
--- Weardian.Client/Core/Services/Symmetric/SymmetricMessageHandlerService.cs
+++ Weardian.Client/Core/Services/Symmetric/SymmetricMessageHandlerService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
-using Weardian.Client.Core.DTOs.MessageHandlerDtos;
+using Weardian.Client.Core.DTOs.MessageHandlerDtos.HandleDecryptionDtos;
+using Weardian.Client.Core.DTOs.MessageHandlerDtos.HandleEncryptionDtos;
 using Weardian.Client.Core.DTOs.WebViewDtos;
 using Weardian.Client.Core.Interfaces.Symmetric;
 
@@ -15,27 +16,77 @@ namespace Weardian.Client.Core.Services.Symmetric
             _keyManagementService = keyManagementService;
         }
 
+        public async Task<string> HandleAsync(string request)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(request))
+                    throw new ArgumentException("Request type cannot be null or empty", nameof(request));
+
+                var requestType = JsonSerializer.Deserialize<WebViewRequestDto>(request)
+                    ?? throw new InvalidOperationException("Failed to deserialized request type");
+
+                return requestType.Type switch
+                {
+                    "encryption" => await HandleEncryptionRequestAsync(request),
+                    "decryption" => await HandleDecryptionRequestAsync(request),
+                    _ => throw new InvalidOperationException("Invalid request type")
+                };
+            }
+            catch (Exception ex)
+            {
+                return JsonSerializer.Serialize(
+                    new WebViewResponseDto<object>(
+                        Type: "Error",
+                        Success: false,
+                        Data: null,
+                        Error: ex.Message));
+            }
+        }
+
         public async Task<string> HandleEncryptionRequestAsync(string request)
         {
             if (string.IsNullOrWhiteSpace(request))
                 throw new ArgumentException("Invalid request: cannot be null, empty or whitespace", nameof(request));
 
-            var result = JsonSerializer.Deserialize<EncryptionRequestDto>(request)
+            var dto = JsonSerializer.Deserialize<EncryptionRequestDto>(request)
                 ?? throw new InvalidOperationException("Deserialization Failed: result cannot be null");
 
             var encryptionResult = await _keyManagementService
                 .CreateEncryptedPasswordAsync(
-                    result.KeyName, 
-                    result.Password, 
-                    result.CreateSynced);
+                    dto.KeyName, 
+                    dto.Password, 
+                    dto.CreateSynced);
 
             return JsonSerializer.Serialize(
-                new WebViewResponseDto<EncryptionResultDto>(
-                    Type: "encryptionResult",
+                new WebViewResponseDto<EncryptionResponseDto>(
+                    Type: "encryption",
                     Success: true,
                     Data: encryptionResult,
                     Error: null
                 ));
+        }
+
+        public async Task<string> HandleDecryptionRequestAsync(string request)
+        {
+            if (string.IsNullOrWhiteSpace(request))
+                throw new ArgumentException("Invalid request: cannot be null, empty or whitespace", nameof(request));
+
+            var dto = JsonSerializer.Deserialize<DecryptionRequestDto>(request);
+
+            if (dto == null || dto.KeyId == Guid.Empty)
+                throw new InvalidOperationException("Deserialization Failed: null result or Guid is invalid.");
+
+            var decryptionResult = await _keyManagementService
+                .RetrieveDecryptedPasswordAsync(dto.KeyId);
+
+            return JsonSerializer.Serialize(
+                new WebViewResponseDto<string>(
+                    Type: "decryption",
+                    Success: true,
+                    Data: decryptionResult,
+                    Error: null
+                    ));
         }
     }
 }
