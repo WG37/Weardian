855bba5b1d30756a12c9b5b95697a3aec0df58d7
WG37 <willg2070@gmail.com>
2026-05-21 20:22:08 +1000

    Refactor DTOs for key management
    Removed `CreateSymmetricKeyRequestDto` and `SymmetricKeyResponseDto` as part of a cleanup. Introduced `KeySyncRequestDto` and `KeySyncResponseDto` to support new key synchronization functionality. Updated relevant `using` directives to reflect these changes.

---
+++ Weardian.Server/Application/DTOs/RequestDtos/KeySyncRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using Weardian.Server.Domain.KeyRecords;
+
+namespace Weardian.Server.Application.DTOs.RequestDtos
+{
+    public sealed record KeySyncRequestDto(
+        Guid EnvelopeId,
+        string Name,
+        KeyType KeyType,
+        int EnvelopeVersion,
+        string WrapAlgorithm,
+        Guid WrappingKeyId,
+        byte[] WrappedKeyNonce,
+        byte[] WrappedKeyCiphertext,
+        byte[] WrappedKeyTag);
+}
---
+++ Weardian.Server/Application/DTOs/ResponseDtos/KeySyncResponseDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Weardian.Server.Application.DTOs.ResponseDtos
+{
+    public sealed record KeySyncResponseDto(
+        Guid EnvelopeId,
+        DateTime SyncedOn);
+}
